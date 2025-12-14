97c2b4931906aed0844a6f3d7046bdcb7c57e59e
KevinSinatria <kevinsinatria8@gmail.com>
2025-12-14 14:55:33 +0700

    feat(services-api): inisialisasi services

---
+++ services/auth.service.ts
@@ -0,0 +1,36 @@
+import { api } from "@/lib/api";
+
+type LoginPayload = {
+  username: string;
+  password: string;
+};
+
+type ChangePasswordPayload = {
+  oldPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+};
+
+const login = async (loginPayload: LoginPayload) => {
+  const res = await api.post("/Auth/login", loginPayload);
+
+  return res.data;
+};
+
+const getProfile = async () => {
+  const res = await api.get("/Auth/profile");
+
+  return res.data;
+};
+
+const changePassword = async (changePasswordPayload: ChangePasswordPayload) => {
+  const res = await api.post("/Auth/change-password", changePasswordPayload);
+
+  return res.data;
+};
+
+export const authService = {
+  login,
+  getProfile,
+  changePassword,
+};
---
+++ services/dashboard.service.ts
@@ -0,0 +1,46 @@
+import { api } from "@/lib/api";
+
+const overviewStats = async () => {
+  const res = await api.get("/Dashboard/stats");
+
+  return res.data;
+};
+
+const todayStats = async () => {
+  const res = await api.get("/Dashboard/today-stats");
+
+  return res.data;
+};
+
+const tapStats = async () => {
+  const res = await api.get("/Dashboard/tap-stats");
+
+  return res.data;
+};
+
+const todayTapStats = async () => {
+  const res = await api.get("/Dashboard/today-tap-stats");
+
+  return res.data;
+};
+
+const monthlyStats = async () => {
+  const res = await api.get("/Dashboard/monthly-stats");
+
+  return res.data;
+};
+
+const last30DayStats = async () => {
+  const res = await api.get("/Dashboard/last-30-day-stats");
+
+  return res.data;
+};
+
+export const dashboardService = {
+  overviewStats,
+  todayStats,
+  tapStats,
+  todayTapStats,
+  monthlyStats,
+  last30DayStats,
+};
